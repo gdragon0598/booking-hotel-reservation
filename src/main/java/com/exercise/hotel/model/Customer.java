8f6e3684d919d7a6f214c1e1b5233193e47e2743
leminhhuan72 <leminhhuan72@gmail.com>
2023-07-06 16:58:07 +0700

    Define custormer model, create test to validate email

---
+++ src/main/java/com/exercise/hotel/model/Customer.java
@@ -0,0 +1,35 @@
+package com.exercise.hotel.model;
+
+import com.exercise.hotel.model.util.CustomerValidate;
+
+import java.io.Serializable;
+
+public class Customer {
+    private final String email;
+    private final String password;
+    private final String firstName;
+    private final String lastName;
+
+    public Customer(String email, String password, String firstName, String lastName) {
+        if (CustomerValidate.emailValidate(email)) {
+            this.email = email;
+        } else {
+            throw new IllegalArgumentException("Email is invalid");
+        }
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
---
+++ src/main/java/com/exercise/hotel/model/util/CustomerValidate.java
@@ -0,0 +1,14 @@
+package com.exercise.hotel.model.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CustomerValidate {
+    private static final String emailRegexPattern = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+            + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+    public static boolean emailValidate(String email) {
+        Pattern pattern = Pattern.compile(emailRegexPattern);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.find();
+    }
+}
